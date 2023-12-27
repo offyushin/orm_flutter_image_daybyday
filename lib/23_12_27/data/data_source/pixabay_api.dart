1d7ef72bcda57b6b1ba507e65f3368e4d0076646
Kim.Yushin <offyushin@naver.com>
2023-12-27 15:39:26 +0900

    23_12_27 Data Source 작성

---
+++ lib/23_12_27/data/data_source/pixabay_api.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import '../dto/PixabayDto.dart';
+import 'package:http/http.dart' as http;
+
+class PixabayApi {
+  Future<PixabayDto> getImagesResult(String query) async {
+    final response = await http.get(Uri.parse('https://pixabay.com/api/?key=41482985-c526f1f433dbd01f22f58e852&q=$query&image_type=photo&pretty=true'));
+    return PixabayDto.fromJson(jsonDecode(response.body));
+  }
+}
