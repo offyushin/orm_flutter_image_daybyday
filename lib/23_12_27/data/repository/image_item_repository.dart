88c2a15f959358f5900d107998bc66ac94928784
Kim.Yushin <offyushin@naver.com>
2023-12-27 15:59:17 +0900

    23_12_27 찐데이타

--- lib/23_12_27/data/repository/image_item_repository.dart
+++ lib/23_12_27/data/repository/image_item_repository.dart
@@ -1,5 +1,24 @@
+import 'package:flutter_image_search_my_app/23_12_27/data/data_source/pixabay_api.dart';
+import 'package:flutter_image_search_my_app/23_12_27/data/mapper/image_mapper.dart';
 import 'package:flutter_image_search_my_app/23_12_27/data/model/image_item.dart';
 
+abstract interface class ImageItemRepository {
+  Future<List<ImageItem>> getImageItems(String query);
+}
+
+class PixabayImageItemRepository implements ImageItemRepository {
+  final _api = PixabayApi();
+
+  Future<List<ImageItem>> getImageItems(String query) async {
+    final dto = await _api.getImagesResult(query);
+
+    if (dto.hits == null) {
+      return [];
+    }
+
+    return dto.hits!.map((e) => e.toImageItem()).toList();
+  }
+}
 class DummyImageItemRepository {
   Future<List<ImageItem>> getImageItems(String query) async {
     await Future.delayed(Duration(seconds: 1));
