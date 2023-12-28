bbfaad8d850fd7b1b9704b7c6dbbf2c2290df7eb
offyushin <offyushin@naver.com>
2023-12-28 16:43:32 +0900

    feat: icons_plus MingCute icon추가, 검색버튼 추가

--- lib/23_12_28/ui/main_screen.dart
+++ lib/23_12_28/ui/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -7,23 +8,39 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          TextField(),
-          Expanded(
-            child: GridView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Text('dddddddddddddddddddddd');
-              },
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 30,
-                mainAxisSpacing: 30,
+          child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            TextField(
+                decoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(32.0),
+                borderSide: const BorderSide(
+                  width: 3,
+                  color: Colors.green,
+                ),
               ),
-            ),
-          )
-        ],
+                  hintText: '검색하세요',
+                  suffixIcon: Icon(
+                      MingCute.search_2_fill,
+                  color: Colors.green,),
+            )),
+            Expanded(
+              child: GridView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Text('dddddddddddddddddddddd');
+                },
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 30,
+                  mainAxisSpacing: 30,
+                ),
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
