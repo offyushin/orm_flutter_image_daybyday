03b1ddf09cbaf9dc04f0ef2c625bbaf7f3a9d811
offyushin <offyushin@naver.com>
2023-12-28 17:00:51 +0900

    feat: imageitem widget 추가

---
+++ lib/23_12_28/ui/widget/imge_item_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_image_search_my_app/23_12_28/data/model/image_item.dart';
+
+
+class ImageItemWidget extends StatelessWidget {
+  final ImageItem imageItem;
+
+  const ImageItemWidget({super.key, required this.imageItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20.0),
+      child: Image.network(
+        imageItem.imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
