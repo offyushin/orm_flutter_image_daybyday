fbe16af73cf29616744c01ce35460aec17bff315
offyushin <offyushin@naver.com>
2023-12-28 18:29:26 +0900

    쉽지않음

--- lib/23_12_28/data/repository/image_item_repository.dart
+++ lib/23_12_28/data/repository/image_item_repository.dart
@@ -1,18 +1,33 @@
-import 'package:flutter_image_search_my_app/23_12_27/data/data_source/pixabay_api.dart';
-import 'package:flutter_image_search_my_app/23_12_27/data/mapper/image_mapper.dart';
-import 'package:flutter_image_search_my_app/23_12_27/data/model/image_item.dart';
+import '../data_source/pixabay_api.dart';
+import '../dto/pixabaydto.dart';
+import '../model/image_item.dart';
 
-abstract interface class ImageItemRepository {}
+abstract interface class ImageItemRepository {
+  Future<List<ImageItem>> getImage(String query);
+}
+
+extension DtoToModel on Hits {
+  ImageItem toImageItem() {
+    return ImageItem(
+      imageUrl: previewURL ??
+          'https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90',
+      tags: tags ?? '',
+    );
+  }
+}
 
 class PixabayImageItemRepository implements ImageItemRepository {
   final _api = PixabayApi();
 
+  @override
   Future<List<ImageItem>> getImage(String query) async {
     final dto = await _api.getImagesResult(query);
 
     if (dto.hits == null) {
-      return [];
+      List<ImageItem> result = [];
+      return result;
     }
+
     return dto.hits!.map((hits) => hits.toImageItem()).toList();
   }
 }
--- lib/23_12_28/ui/main_screen.dart
+++ lib/23_12_28/ui/main_screen.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search_my_app/23_12_28/data/model/image_item.dart';
+import 'package:flutter_image_search_my_app/23_12_28/ui/widget/imge_item_widget.dart';
 import 'package:icons_plus/icons_plus.dart';
 
-class MainScreen extends StatelessWidget {
+import '../data/repository/image_item_repository.dart';
+
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final searchText = TextEditingController();
+
+  @override
+  void dispose() {
+    searchText.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,40 +30,56 @@ class MainScreen extends StatelessWidget {
         child: Column(
           children: [
             TextField(
-                decoration: InputDecoration(
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(32.0),
-                borderSide: const BorderSide(
-                  width: 4,
-                  color: Colors.green,
+              controller: searchText,
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                  borderSide: const BorderSide(
+                    width: 4,
+                    color: Colors.green,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(24.0),
+                  borderSide: const BorderSide(
+                    width: 5,
+                    color: Colors.green,
+                  ),
+                ),
+                hintText: '검색하세요',
+                suffixIcon: IconButton(
+                  icon: const Icon(
+                    MingCute.search_2_fill,
+                    color: Colors.green,
+                  ),
+                  onPressed: () => setState(() {}),
                 ),
               ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(24.0),
-                borderSide: BorderSide(
-                  width: 5,
-                  color: Colors.green,
-                )
-              ),
-              hintText: '검색하세요',
-              suffixIcon: Icon(
-                MingCute.search_2_fill,
-                color: Colors.green,
-              ),
-            )),
-            Expanded(
-              child: GridView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Text('dddddddddddddddddddddd');
-                },
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 30,
-                  mainAxisSpacing: 30,
-                ),
-              ),
-            )
+            ),
+            const SizedBox(height: 30),
+            FutureBuilder<List<ImageItem>>(
+                future: PixabayImageItemRepository().getImage(searchText.text),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+                  final imageItems = snapshot.data!;
+                  return Expanded(
+                    child: GridView.builder(
+                      itemCount: imageItems.length,
+                      itemBuilder: (context, index) {
+                        final imageItem = imageItems[index];
+                        return ImageItemWidget(imageItem: imageItem);
+                      },
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 30,
+                        mainAxisSpacing: 30,
+                      ),
+                    ),
+                  );
+                })
           ],
         ),
       )),
