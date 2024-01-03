8139abaa38eb88bead241595e125434cb98b5c64
offyushin <offyushin@naver.com>
2024-01-03 18:34:50 +0900

    feat: main_screen 가짜 데이터 연결

--- lib/24_01_03/ui/main_screen.dart
+++ lib/24_01_03/ui/main_screen.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_image_search_my_app/24_01_03/data/model/image_item.dart';
+import 'package:flutter_image_search_my_app/24_01_03/data/repository/image_item_repository.dart';
 import 'package:flutter_image_search_my_app/24_01_03/ui/widget/image_item_widget.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final repository = ImageItemREpositoy();
+  var imageItems = [];
+
+  Future<void> searchImage(String query) async {
+    imageItems = await repository.getImageItems(query);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,42 +30,42 @@ class MainScreen extends StatelessWidget {
                 decoration: InputDecoration(
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(
+                    borderSide: const BorderSide(
                       width: 2,
                       color: Colors.black,
                     ),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(
+                    borderSide: const BorderSide(
                       width: 2,
                       color: Colors.grey,
                     ),
                   ),
                   hintText: '검색',
-                  suffixIcon: Icon(
-                    Icons.search,
-                    color: Colors.black,
+                  suffixIcon: IconButton(
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    ),
+                    onPressed: () => searchImage('사과'),
                   ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 24,
               ),
               Expanded(
                 child: GridView.builder(
-                    itemCount: 10,
+                    itemCount: imageItems.length,
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
                             crossAxisCount: 2),
                     itemBuilder: (context, index) {
-                      final imageItem = ImageItem(
-                        imageUrl: 'https://cdn.pixabay.com/photo/2023/12/06/08/41/mountain-8433234_1280.jpg',
-                        tags: 'mountain',
-                      );
+                      final imageItem = imageItems[index];
                       return ImageItemWidget(imageItem: imageItem);
                     }),
-              )
+              ),
             ],
           ),
         ),
