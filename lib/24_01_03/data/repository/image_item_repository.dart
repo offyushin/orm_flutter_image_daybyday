e96678fda627f870038180c00544478de87c735a
offyushin <offyushin@naver.com>
2024-01-03 19:01:53 +0900

    feat: image_item_repository 진짜 데이터

--- lib/24_01_03/data/repository/image_item_repository.dart
+++ lib/24_01_03/data/repository/image_item_repository.dart
@@ -1,24 +1,21 @@
+import 'package:flutter_image_search_my_app/24_01_03/data/data_source/pixabay_api.dart';
+import 'package:flutter_image_search_my_app/24_01_03/data/mapper/image_mapper.dart';
 import 'package:flutter_image_search_my_app/24_01_03/data/model/image_item.dart';
 
-class ImageItemREpositoy {
+abstract interface class ImageItemRepository {
+  Future<List<ImageItem>> getImageItems(String query);
+}
+
+class PixabayImageItemRepository implements ImageItemRepository {
+  final _api = PixabayApi();
+
+  @override
   Future<List<ImageItem>> getImageItems(String query) async {
-    return [
-      ImageItem(
-          imageUrl:
-              'https://cdn.pixabay.com/photo/2023/12/06/08/41/mountain-8433234_1280.jpg',
-          tags: ''),
-      ImageItem(
-          imageUrl:
-              'https://cdn.pixabay.com/photo/2023/10/20/19/25/moon-8330104_640.png',
-          tags: ''),
-      ImageItem(
-          imageUrl:
-              'https://cdn.pixabay.com/photo/2023/11/26/17/52/flowers-8414039_640.jpg',
-          tags: ''),
-      ImageItem(
-          imageUrl:
-              'https://cdn.pixabay.com/photo/2023/11/02/10/19/bird-8360220_640.jpg',
-          tags: ''),
-    ];
+    final dto = await _api.getImagesResult(query);
+
+    if (dto.hits == null) {
+      return [];
+    }
+    return dto.hits!.map((e) => e.toImageItem()).toList();
   }
 }
