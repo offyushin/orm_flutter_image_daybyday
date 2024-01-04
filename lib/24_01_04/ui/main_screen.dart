aa616d55b1540c07772712a436577fb9a833c436
offyushin <offyushin@naver.com>
2024-01-04 18:22:32 +0900

    feat: model

--- lib/24_01_04/ui/main_screen.dart
+++ lib/24_01_04/ui/main_screen.dart
@@ -5,6 +5,24 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const TextField(),
+            Expanded(
+              child: GridView.builder(
+                  itemCount: 30,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return const Text('offyushin');
+                  },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
